baac024717a770bf51f6cb97750ebd630e2923ea
14budig <nbudig@gmail.com>
2016-09-21 10:04:05 -0700

    creates basic file structure and sets up server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+
+app.get('/', function homepage(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+var key = 'sfdugjby7k6cfxvpuyr5b6mkmvghwmr4';
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/');
+});
